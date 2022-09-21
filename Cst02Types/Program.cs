1608c981ad66a6a116836de877bb0f576a65b742
Michal Stehlik <michal.stehlik@pslib.cz>
2022-09-21 14:48:05 +0200

    data types

---
+++ Cst02Types/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Hodnotové typy 
+ */
+
+// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/value-types
+
+// implicitní přetypování
+float a = 10f;
+double b = a;
+Console.WriteLine(b);
+
+// explicitní přetypování
+double c = 10;
+float d = (float)c;
+
+int e = 5;
+double f = e;
+
+double g = 5.4;
+int h = (int)g;
+Console.WriteLine(h);
+
+// konverze
+string i = "Veverka";
+int j;
+bool ok = int.TryParse(i, out j);
+Console.WriteLine(j);
+
+//int k = int.Parse(i);
+//Console.WriteLine(k);
+
+//int l = Convert.ToInt32(i);
+//Console.WriteLine(l);
+
+// camelCase - proměnné
+// PascalCase - funkce a metody
+// under_line - //
+// UPPER_CASE - důležité konstanty
