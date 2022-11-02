4ec06a8f08835c821b03f989ee32e8c2d115c002
MichalStehlik <michal.stehlik@pslib.cz>
2022-11-02 15:05:10 +0100

    objects

---
+++ Cst05objects/Program.cs
@@ -0,0 +1,22 @@
+﻿using Cst05objects;
+
+Student adam = new Student("Adam","Kos",2, 2.3);
+/*
+adam.firstname = "Adam";
+Console.WriteLine(adam.firstname);
+Console.WriteLine(adam.lastname);
+adam.firstname = "Adolf";
+Console.WriteLine(adam.firstname);
+*/
+Console.WriteLine(adam.GetFirstname());
+Console.WriteLine(adam.GetGrade());
+adam.Advance();
+adam.Advance();
+adam.Advance();
+adam.Advance();
+Console.WriteLine(adam.GetGrade());
+adam.SetGrade(1);
+Console.WriteLine(adam.GetGrade());
+Student fred = new Student();
+Console.WriteLine(fred.GetGrade());
+Console.WriteLine(fred);
---
+++ Cst05objects/Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cst05objects
+{
+    internal class Student : object
+    {
+        private string firstname; // atributy, pasivní data, stav objektu
+        private string lastname;
+        private int grade = 1;
+        private double markAgerage;
+
+        // konstruktor
+        public Student(string firstname, string lastname, int grade, double markAgerage)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.grade = grade;
+            this.markAgerage = markAgerage;
+        }
+
+        public Student()
+        {
+            firstname = "Fred";
+            lastname = "Skočdopole";
+            markAgerage = 5;
+        }
+
+        public string GetFirstname()
+        {
+            return firstname;
+        }
+        public int GetGrade() // getter
+        {
+            return grade;
+        }
+
+        public int Advance()
+        {
+            if (grade < 4) { return ++grade; } else { return grade; }
+            
+        }
+
+        //setter
+        public void SetGrade(int value) // metoda, aktivní, mění stav objektu, přistupuje k privátním členům
+        {
+            if (value > 0 && value <= 4)
+                grade = value;
+        }
+
+        public string GetFullname()
+        {
+            return firstname + " " + lastname;
+        }
+
+        public override string ToString()
+        {
+            return firstname + " " + lastname + " " + grade;
+        }
+
+    }
+}
