7d6f2f3046be25f1a5b0e56cd0956bed32191469
MichalStehlik <michal.stehlik@pslib.cz>
2022-12-07 14:52:14 +0100

    operators overloading

---
+++ Cst10OperatorOverloading/Number.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cst10OperatorOverloading
+{
+    internal class Number
+    {
+        public Number(double value)
+        {
+            Value = value;
+        }
+
+        public double Value { get; set; }
+
+        public static Number operator + (Number a, int value)
+        {
+            return new Number(a.Value + value);
+        }
+        public static Number operator +(Number a, Number b)
+        {
+            return new Number(a.Value + b.Value);
+        }
+        public static Number operator +(int value, Number b)
+        {
+            return new Number(value + b.Value);
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
---
+++ Cst10OperatorOverloading/Program.cs
@@ -0,0 +1,10 @@
+﻿using Cst10OperatorOverloading;
+
+Number a = new Number(1);
+Number b = new Number(2);
+Number c = a + 1;
+Console.WriteLine(c);
+Number d = a + b;
+Console.WriteLine(d);
+Number e = 1 + a + 2 + b;
+Console.WriteLine(e);
