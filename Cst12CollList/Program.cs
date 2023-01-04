58576c5249711b84b990ea8d4c59307023b36383
MichalStehlik <michal.stehlik@pslib.cz>
2023-01-04 14:58:38 +0100

    Dictionary ArrayList

--- Cst12CollList/Program.cs
+++ Cst12CollList/Program.cs
@@ -1,4 +1,19 @@
 ﻿using Cst12CollList;
+using System.Collections;
+
+ArrayList al = new ArrayList();
+al.Add(1);
+al.Add("Ahoj");
+al.Add(new Student { Firstname = "Alois" });
+al.Add(new Student { Firstname = "Bořivoj" });
+al.Add(new Student { Firstname = "Ctirad" });
+foreach (var x in al)
+{
+    if (x is Student)
+    {
+        Console.WriteLine((x as Student).Firstname);
+    } 
+}
 
 List<int> cisla = new List<int>();
 cisla.Add(1);
@@ -7,6 +22,7 @@ cisla.Add(3);
 cisla.Add(5);
 cisla.Add(6);
 cisla.Add(9);
+cisla.Insert(0,8);
 cisla[2] = 8;
 cisla.Remove(2);
 cisla.RemoveAt(3);
@@ -15,3 +31,24 @@ foreach(var x in cisla)
     Console.WriteLine(x);
 }
 var r = new Repository<int>();
+
+Dictionary<string, string> slovicka = new Dictionary<string, string>();
+slovicka.Add("pes","dog");
+slovicka.Add("kočka", "cat");
+slovicka.Add("myš", "mouse");
+Console.WriteLine(slovicka["pes"]);
+if (slovicka.ContainsKey("antilopa"))
+{
+    Console.WriteLine(slovicka["antilopa"]); // exception
+}
+else
+{
+    Console.WriteLine("--");
+}
+//slovicka.Add("kočka", "kitty");
+slovicka["kočka"] = "kitty";
+slovicka.Remove("myš");
+foreach(var x in slovicka)
+{
+    Console.WriteLine(x.Key + ":" + x.Value);
+}
---
+++ Cst12CollList/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cst12CollList
+{
+    internal class Student
+    {
+        public string Firstname { get; set; }
+    }
+}
