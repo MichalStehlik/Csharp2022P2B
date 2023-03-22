5484975c97183a89a26d326c5c07b867341aface
MichalStehlik <michal.stehlik@pslib.cz>
2023-03-22 14:59:12 +0100

    filesystem

---
+++ Cst20RegEx/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+/*
+string[] partNumbers = { "1298-673-4192", "A08Z-931-468a", "_A90-123-129X", "12345-KKA-1230", "0919-2893-1256" };
+string pattern = @"^[A-Z0-9]\d{2}[A-Z0-9](-\d{3}){2}[A-Z0-9]$";
+foreach (string partNumber in partNumbers)
+{
+    if (Regex.IsMatch(partNumber, pattern, RegexOptions.IgnoreCase))
+        Console.WriteLine("Yes");
+    else
+        Console.WriteLine("No");
+}
+*/
+/*
+string pattern = @"\ba\w*\b";
+string input = "The extraordinary day dawns with any new day.";
+Match m = Regex.Match(input, pattern, RegexOptions.IgnoreCase);
+if (m.Success)
+    Console.WriteLine("Found '{0}' at position {1}.", m.Value, m.Index);
+else
+    Console.WriteLine("---"); // <--
+*/
+/*
+string pattern = @"\b\w+es\b"; // slovo obsahující písmena "es" - [mezera][slovo]es[mezera]
+string sentence = "NOTES: Any notes or comments are optional.";
+foreach (Match match in Regex.Matches(sentence, pattern, RegexOptions.None, TimeSpan.FromSeconds(1)))
+{
+    Console.WriteLine("Found '{0}' at position {1}", match.Value, match.Index); // notes
+}
+*/
---
+++ Cst21filesystem/Program.cs
@@ -0,0 +1,47 @@
+﻿File.Create("D:\\data.txt");
+if (File.Exists("D:\\data.txt")) Console.WriteLine("Exists");
+Console.ReadKey();
+try
+{
+    File.Delete("D:\\data.txt");
+}
+catch
+{
+    Console.WriteLine("Bum");
+}
+var f =  File.GetAttributes("D:\\Moderní písmo.pptx").ToString();
+Console.WriteLine(f);
+string soubor = @"D:\\Moderní písmo.pptx";
+Console.WriteLine("Čas vytvoření souboru je: " + File.GetCreationTime(soubor));
+Console.WriteLine("Čas posledního přístupu k souboru je: " + File.GetLastAccessTime(soubor));
+Console.WriteLine("Čas posledního zápisu do souboru je: " + File.GetLastWriteTime(soubor));
+FileInfo fi = new FileInfo(soubor);
+long velikostSouboru = fi.Length;
+Console.WriteLine(velikostSouboru);
+Console.ReadKey();
+string rodicovskaSlozka = @"D:\IKT-E1A";
+var d = Directory.GetDirectories(rodicovskaSlozka, "*", SearchOption.AllDirectories);
+foreach (var x in d)
+{
+    Console.WriteLine(x);
+};
+string[] s = Directory.GetFiles(rodicovskaSlozka);
+foreach (var x in s)
+{
+    Console.WriteLine(x);
+}
+
+DriveInfo[] allDrives = DriveInfo.GetDrives();
+foreach (DriveInfo dr in allDrives)
+{
+    Console.WriteLine("Drive {0}", dr.Name);
+    Console.WriteLine("  Drive type: {0}", dr.DriveType);
+    if (dr.IsReady == true)
+    {
+        Console.WriteLine("  Volume label: {0}", dr.VolumeLabel);
+        Console.WriteLine("  File system: {0}", dr.DriveFormat);
+        Console.WriteLine("  Available space to current user:{0, 15} bytes", dr.AvailableFreeSpace);
+        Console.WriteLine("  Total available space:          {0, 15} bytes", dr.TotalFreeSpace);
+        Console.WriteLine("  Total size of drive:            {0, 15} bytes ", dr.TotalSize);
+    }
+}
